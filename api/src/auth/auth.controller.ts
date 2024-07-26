f8f33b3c19fcb452fd9a5c075fc801376ddda15e
henriqueSoto92 <henriquesoto92@gmail.com>
2024-07-25 21:51:53 -0300

    feat(52): multiple upload

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UploadedFiles,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
@@ -15,7 +16,11 @@ import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { User } from 'src/decorators/user.decorator';
-import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+  FilesInterceptor,
+} from '@nestjs/platform-express';
 import { join } from 'path';
 import { FileService } from 'src/file/file.service';
 
@@ -72,6 +77,31 @@ export class AuthController {
     }
 
     return { sucess: true };
+  }
+  @UseInterceptors(FilesInterceptor('files'))
+  @UseGuards(AuthGuard)
+  @Post('files')
+  async uploadFiles(
+    @User() user,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    return { sucess: files };
+  }
+
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'photo', maxCount: 1 },
+      { name: 'documents', maxCount: 1 },
+    ]),
+  )
+  @UseGuards(AuthGuard)
+  @Post('files-fields')
+  async uploadFilesFields(
+    @User() user,
+    @UploadedFiles()
+    files: { photo: Express.Multer.File; documents: Express.Multer.File },
+  ) {
+    return files;
   }
   // async me(@Headers('authorization') token) {
   //   // return (token ?? '').split(' ')[1];
